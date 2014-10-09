95264c996b9d4a8cb81dec41e75f1ea50cb92b49
Dominic Perotti <dominic@Dominic-Perottis-MacBook-Pro.local>
2014-10-09 10:59:00 -0400

    updated class data

--- models.py
+++ models.py
@@ -4,12 +4,48 @@ class Course(object):
     def __init__(self, course_name, seats):
         self.course_name = course_name
         self.seats = seats
+    	self.students = []
+
+    def addStudent(Student):
+	self.students.append(Student)
+    
+    def removeStudent(Student):
+	self.students.remove(Student)
+
+    def getStudents():
+	return self.students
+
+    def setSeats(numSeats):
+	self.seats = numSeats
+
+    def isFull():
+	if len(self.students) == len(self.students):
+		return true
+	else:
+		return false
 
 class Student(object):
-    def __init__(self, name, email, pref_one=None, pref_two=None, pref_three=None):
+    def __init__(self, name, email, time_submitted, pref_one=None, pref_two=None, pref_three=None):
         self.name = name
-        self.email = email
+	#dont worry about email till we have confirmation from mellinger
+	self.email = email
         self.preferences = []
         self.raw_preferences = []
         # loop through and process the preferences, putting each into the raw preferences
         # and parsing to get the course name into the preferences.
+	self.time_submitted = time_submitted
+    
+    def getPreferences():
+	return self.preferences
+
+    def getRawPreferences():
+	return self.raw_preferences
+
+    def getName():
+	return self.name
+
+    def getEmail()
+	return self.email
+
+    def getTimeSubmitted()
+	return self.time_submitted
